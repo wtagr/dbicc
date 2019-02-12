c58cc04e8841fa83b91addb0b2f0fa41a7bae943
meng <xumeng818@gmail.com>
2019-02-12 10:11:19 +0200

    R package

---
+++ R/dm2icc.R
@@ -0,0 +1,58 @@
+#' Distance-based Intraclass Correlation Coefficient (dbICC)
+#' 
+#' This is a function to calculate the value of dbICC through the related distance matrix. 
+#' For example, when the distance among any test-retest datatype can be defined, its reliability 
+#' performed in DRAFT can be obtained by this function. A sample of the certain subjects with 
+#' the replacement, which is used in the boostrap confidence intervals, can be reached as well.  
+#' 
+#' 
+#' @param dmat A distance matrix, of dimension \code{sum(nmea)*sum(nmea)}. Note that the order of the rows or
+#' columns in the distance matrix is sorted by groups of measurements for each subject or individual. The details refer to
+#' Reiss el at., 2019.
+#' @param nsub Number of the subject or individual.
+#' @param nmea A vector containing number of the measurement for each subject or individual; 
+#' if \code{nmea} is a scalar, it means each subject shares the same number of the measurement.
+#' @param bootsamp option to a vector requiring a sample with the replacement. Default 
+#' includes all the elements of the specified subjects or individuals.
+#' @param adhoc option to a logical variable: whether to apply the ad hoc correction when
+#' estimating the dbICC from a bootstrap sample. Default is \code{FALSE}.
+#' @return A scalar, giving the dbICC value
+#' @note %% ~~further notes~~
+#' 
+#' @author Philip Reiss \email{reiss@@stat.haifa.ac.il}, Meng Xu \email{mxu@@campus.haifa.ac.il}
+#' 
+#' @seealso \code{\link{plotdmat}}
+#' 
+#' @references
+#' \itemize{
+#' \item Reiss et al. (2019). Generalized test-retest reliability based on distances. 
+#' }
+#' 
+#' @keywords dbICC, distance matrix, reliability
+#' @import Matrix
+#' 
+#' @examples
+#' #under construction
+#' 
+dm2icc <-
+function(dmat, nsub, nmea, bootsamp = 1:nsub, adhoc = FALSE) {
+  #nmea: the number of the measurement
+  if (length(nmea)==1) nmea<-rep(nmea,nsub)
+  wmask <- as.matrix(Matrix::bdiag(lapply(nmea, function(i) matrix(1, i, i))))
+  diag(wmask) <- NA
+  bmask <- matrix(1, sum(nmea), sum(nmea)) - wmask
+  wmask[wmask == 0] <- bmask[bmask == 0] <- NA
+  bmask.ah <- bmask
+  if (!identical(bootsamp, 1:nsub)){
+    wibs <- sapply(bootsamp, function(i) sum(nmea[1:i - 1]) + 1:nmea[i])
+    dboot <- dmat[wibs, wibs]
+    if (adhoc) {
+      for (ii in 1:(nsub - 1))
+        for (jj in (ii + 1):nsub) {
+          if (bootsamp[ii] == bootsamp[jj]) bmask.ah[(sum(nmea[1:ii - 1]) + 1:nmea[ii]), (sum(nmea[1:jj - 1]) + 1:nmea[jj])] <- bmask.ah[(sum(nmea[1:jj - 1]) + 1:nmea[jj]), (sum(nmea[1:ii - 1]) + 1:nmea[ii])] <- NA
+        }
+    }
+  }
+  else dboot<-dmat
+  1 - mean(dboot * wmask, na.rm = TRUE) / mean(dboot * bmask.ah, na.rm = TRUE)
+}
---
+++ R/plotdmat.R
@@ -0,0 +1,56 @@
+#' Plot the distance matrix in dbICC 
+#' 
+#' This function is to plot the distance matrix in dbICC including the segmentation boundary (colored in red) between the within-
+#' distance and between-distance based on \code{ggplot2}.
+#' 
+#' 
+#' @param dmat A distance matrix
+#' @param nsub Number of the subject or individual
+#' @param nmea A vector of number of the measurement for each subject or individual
+#' @param xlab The title for the x axis
+#' 
+#' @return A ggplot object
+#' 
+#' @note %% ~~further notes~~
+#' 
+#' @author Philip Reiss \email{reiss@@stat.haifa.ac.il}, Meng Xu \email{mxu@@campus.haifa.ac.il}
+#' 
+#' @seealso \code{\link{dm2icc}}
+#' 
+#' @references 
+#' \itemize{
+#' \item Reiss et al. (2019). Generalized test-retest reliability based on distances. 
+#' }
+#' @keywords
+#' 
+#' @import ggplot2
+#' @import reshape2
+#' @examples
+#' #under construction
+#' 
+plotdmat <-
+function(dmat, nsub, nmea,xlab=NULL){
+    # plot dist matrix with boundary between within and between distance 
+    if (length(nmea)==1) nmea=rep(nmea,nsub)
+    blkfun<-function(i) sum(nmea[1:i-1])+c(1,nmea[i])
+    blkm<-function(i) c(0,-nmea[i]+1)
+    xyl<-unlist(lapply(1:nsub,blkfun))
+    ym<-unlist(lapply(1:nsub,blkm))
+    xyl<-rbind(xyl,xyl+ym)+matrix(rep(c(-.5,-.5,0.5,-0.5),times=nsub),2)
+    xyl<-t(xyl)
+    colnames(xyl)<-c("x","y")
+    
+    A<-melt(dmat)
+    colnames(A)<-c('x','y','value')
+    B<-data.frame(xyl)
+    p<-ggplot(A,aes(x,y))+geom_raster(aes(fill=value))+
+      geom_path(data=B,aes(x,y),color="red")+
+      geom_path(data=B,aes(y,x),color="red")+xlab(xlab)+
+      theme(legend.position = "none",
+            panel.grid = element_blank(),
+            axis.title.y = element_blank(),
+            axis.title.x=element_text(size=18),
+            axis.text = element_blank(),
+            axis.ticks = element_blank())+scale_y_reverse() 
+    return(p)
+}
