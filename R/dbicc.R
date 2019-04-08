c283f3ce43132cb8e8679a093fc926e31966e89b
meng <xumeng818@gmail.com>
2019-04-08 15:33:41 +0300

    0.1

--- R/dbicc.R
+++ R/dbicc.R
@@ -1,6 +1,8 @@
 #' Distance-based Intraclass Correlation Coefficient (dbICC)
 #' 
+#' Please move to \code{\link{dm2icc}}
 #' 
 #' @export
 #' @seealso \code{\link{dm2icc}}
 #' 
+dbicc <- function() {}
